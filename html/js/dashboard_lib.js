f44a87c1a44f997943bd54d3b425cec2cc6661c4
itoy372 <yuki.ito09@g.softbank.co.jp>
2018-12-11 15:33:59 +0900

    threeの不要な処理を削除

--- html/js/dashboard_lib.js
+++ html/js/dashboard_lib.js
@@ -1,17 +1,8 @@
-AmCharts.translations.dataLoader={},AmCharts.addInitHandler(function(a){function e(b,d,g,h){void 0===h&&(h="dataProvider"),g.showCurtain&&l(void 0,g.noStyles),c.remaining++,c.percentLoaded[b]=0,void 0!==g.progress&&"function"==typeof g.progress&&void 0===g._progress&&(g._progress=g.progress,g.progress=function(a){c.percentLoaded[b]=a;var d=0,e=0;for(var f in c.percentLoaded)c.percentLoaded.hasOwnProperty(f)&&(e++,d+=c.percentLoaded[f]);var h=Math.round(d/e*100)/100;g._progress.call(this,h,Math.round(100*a)/100,b)}),AmCharts.loadFile(b,g,function(i){if(!1===i)n(g.error,g,a),k(AmCharts.__("Error loading the file",a.language)+": "+b,!1,g);else{switch(void 0===g.format&&(g.format="json"),g.format=g.format.toLowerCase(),g.format){case"json":if(d[h]=AmCharts.parseJSON(i),!1===d[h])return n(g.error,g,a),k(AmCharts.__("Error parsing JSON file",a.language)+": "+c.url,!1,g),void(d[h]=[]);d[h]=f(d[h],g),n(g.load,g,a);break;case"csv":if(d[h]=AmCharts.parseCSV(i,g),!1===d[h])return n(g.error,g,a),k(AmCharts.__("Error parsing CSV file",a.language)+": "+c.url,!1,g),void(d[h]=[]);d[h]=f(d[h],g),n(g.load,g,a);break;default:return n(g.error,g,a),void k(AmCharts.__("Unsupported data format",a.language)+": "+g.format,!1,g.noStyles)}if(c.remaining--,0===c.remaining&&(n(g.complete,a),g.async))if("map"===a.type)a.validateNow(!0),m();else if("gauge"!==a.type&&a.addListener("dataUpdated",function(b){"stock"!==a.type||g.reloading||void 0===a.periodSelector||a.periodSelector.setDefaultPeriod(),m(),a.events.dataUpdated.pop()}),a.validateData(),"gauge"===a.type&&m(),c.startDuration)if("stock"===a.type){a.panelsSettings.startDuration=c.startDuration;for(var j=0;j<a.panels.length;j++)a.panels[j].startDuration=c.startDuration,a.panels[j].animateAgain()}else a.startDuration=c.startDuration,void 0!==a.animateAgain&&a.animateAgain();g.reload&&(g.timeout&&clearTimeout(g.timeout),g.timeout=setTimeout(e,1e3*g.reload,b,d,g),g.reloading=!0)}})}function f(b,d){if(void 0===d.postProcess||!h(d.postProcess))return b;try{return d.postProcess.call(c,b,d,a)}catch(e){return k(AmCharts.__("Error loading file",a.language)+": "+d.url,!1,d),b}}function g(a){return"object"==typeof a}function h(a){return"function"==typeof a}function i(a){for(var b in d)d.hasOwnProperty(b)&&j(a,b,d[b])}function j(a,b,c){void 0===a[b]&&(a[b]=c)}function k(a,b,c){c.showErrors?l(a,c.noStyles):(m(),console.log(a))}function l(b,d){m(),void 0===b&&(b=AmCharts.__("Loading data...",a.language));var e=document.createElement("div");if(e.setAttribute("id",a.div.id+"-curtain"),e.className="amcharts-dataloader-curtain",!0!==d){e.style.position="absolute",e.style.top=0,e.style.left=0,e.style.width=(void 0!==a.realWidth?a.realWidth:a.divRealWidth)+"px",e.style.height=(void 0!==a.realHeight?a.realHeight:a.divRealHeight)+"px",e.style.textAlign="center",e.style.display="table",e.style.fontSize="20px";try{e.style.background="rgba(255, 255, 255, 0.3)"}catch(f){e.style.background="rgb(255, 255, 255)"}e.innerHTML='<div style="display: table-cell; vertical-align: middle;">'+b+"</div>"}else e.innerHTML=b;a.containerDiv.appendChild(e),c.curtain=e}function m(){try{void 0!==c.curtain&&a.containerDiv.removeChild(c.curtain)}catch(b){}c.curtain=void 0}function n(a,b,d,e){"function"==typeof a&&a.call(c,b,d,e)}void 0!==a.dataLoader&&g(a.dataLoader)||(a.dataLoader={});var b=a.version.split(".");if(!(Number(b[0])<3||3===Number(b[0])&&Number(b[1])<13)){var c=a.dataLoader;c.remaining=0,c.percentLoaded={};var d={async:!0,format:"json",showErrors:!0,showCurtain:!0,noStyles:!1,reload:0,timestamp:!1,delimiter:",",skip:0,skipEmpty:!0,emptyAs:void 0,useColumnNames:!1,init:!1,progress:!1,reverse:!1,reloading:!1,complete:!1,error:!1,numberFields:[],headers:[],chart:a};c.loadData=function(){if("stock"===a.type)setTimeout(function(){0>a.panelsSettings.startDuration&&(c.startDuration=a.panelsSettings.startDuration,a.panelsSettings.startDuration=0);for(var b=0;b<a.dataSets.length;b++){var d=a.dataSets[b];void 0!==d.dataLoader&&void 0!==d.dataLoader.url&&(n(d.dataLoader.init,d.dataLoader,a),d.dataProvider=[],i(d.dataLoader),e(d.dataLoader.url,d,d.dataLoader,"dataProvider")),void 0!==d.eventDataLoader&&void 0!==d.eventDataLoader.url&&(n(d.eventDataLoader.init,d.eventDataLoader,a),d.events=[],i(d.eventDataLoader),e(d.eventDataLoader.url,d,d.eventDataLoader,"stockEvents"))}},100);else{if(n(c.init,c,a),i(c),void 0===c.url)return;void 0!==a.startDuration&&0<a.startDuration&&(c.startDuration=a.startDuration,a.startDuration=0),"gauge"===a.type?(void 0===a.arrows&&(a.arrows=[]),e(c.url,a,c,"arrows")):(void 0===a.dataProvider&&(a.dataProvider="map"===a.type?{}:[]),e(c.url,a,c,"dataProvider"))}},c.loadData()}},["pie","serial","xy","funnel","radar","gauge","gantt","stock","map"]),void 0===AmCharts.__&&(AmCharts.__=function(a,b){return void 0!==b&&void 0!==AmCharts.translations.dataLoader[b]&&void 0!==AmCharts.translations.dataLoader[b][a]?AmCharts.translations.dataLoader[b][a]:a}),AmCharts.loadFile=function(a,b,c){"object"!=typeof b&&(b={}),void 0===b.async&&(b.async=!0);var d;d=window.XMLHttpRequest?new XMLHttpRequest:new ActiveXObject("Microsoft.XMLHTTP");try{d.open("GET",b.timestamp?AmCharts.timestampUrl(a):a,b.async)}catch(e){c.call(this,!1)}if(void 0!==b.headers&&b.headers.length)for(var f=0;f<b.headers.length;f++){var g=b.headers[f];d.setRequestHeader(g.key,g.value)}void 0!==b.progress&&"function"==typeof b.progress&&(d.onprogress=function(a){var c=a.loaded/a.total*100;b.progress.call(this,c)}),d.onreadystatechange=function(){4===d.readyState&&404===d.status?c.call(this,!1):4===d.readyState&&200===d.status&&c.call(this,d.responseText)};try{d.send()}catch(e){c.call(this,!1)}},AmCharts.parseJSON=function(response){try{return void 0!==JSON?JSON.parse(response):eval(response)}catch(e){return!1}},AmCharts.parseCSV=function(a,b){var g,h,c=AmCharts.CSVToArray(a,b.delimiter),d=b.numberFields&&b.numberFields.length>0,e=[],f=[];if(b.useColumnNames){f=c.shift();for(var i=0;i<f.length;i++)g=f[i].replace(/^\s+|\s+$/gm,""),""===g&&(g="col"+i),f[i]=g;0<b.skip&&b.skip--}for(h=0;h<b.skip;h++)c.shift();for(var j;j=b.reverse?c.pop():c.shift();)if(!b.skipEmpty||1!==j.length||""!==j[0]){var k={};for(h=0;h<j.length;h++)g=void 0===f[h]?"col"+h:f[h],k[g]=""===j[h]?b.emptyAs:j[h],d&&-1!==b.numberFields.indexOf(g)&&(k[g]=Number(k[g]));e.push(k)}return e},AmCharts.CSVToArray=function(a,b){b=b||",";for(var c=new RegExp("(\\"+b+'|\\r?\\n|\\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^"\\'+b+"\\r\\n]*))","gi"),d=[[]],e=null;e=c.exec(a);){var f=e[1];f.length&&f!==b&&d.push([]);var g;g=e[2]?e[2].replace(new RegExp('""',"g"),'"'):e[3],d[d.length-1].push(g)}return d},AmCharts.timestampUrl=function(a){var b=a.split("?");return 1===b.length?b[1]=(new Date).getTime():b[1]+="&"+(new Date).getTime(),b.join("?")};
-
 //クラス定義
 var ThreeDD = {};
 ThreeDD.data;
 ThreeDD.finished = false;
 ThreeDD.main = function(){
-	
-	//Lifrayの自動変換対応
-	if(location.href.indexOf("js_fast_load") < 0){
-		var url = location.href + "?js_fast_load=0";
-		location.href = url;
-		return
-	}
 	
 	//外部CSS読込
 	function loadCss(href, check) {
@@ -48,8 +39,8 @@ ThreeDD.main = function(){
 	}
 	
 	function MakePath(str){
-// 		return site_settings.graphInfoServiceSSL + site_settings.graphInfoServiceIP + "/BigScreen/" + str;
 		return "/html/" + str;
+		return site_settings.graphInfoServiceSSL + site_settings.graphInfoServiceIP + "/BigScreen/" + str;
 	}
 	
 	var GetUrlVars = function(){
@@ -133,22 +124,7 @@ ThreeDD.main = function(){
 		controls.maxDistance = 6000 * resolutionZoom;
 		controls.addEventListener('change', ReversalPanel);
 
-		scene = new THREE.Scene();
-
 		geometry = new THREE.CubeGeometry( 200, 300, 400 );
-		
-		material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1 } );
-		mesh = new THREE.Mesh( geometry, material );
-		mesh.position.set(50,100,70)
-		mesh.rotation.x = Math.PI/3
-		mesh.rotation.y = Math.PI/4
-		mesh.rotation.z = Math.PI/5
-
-		scene.add( mesh );
-
-		renderer = new THREE.CanvasRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.getElementById('container').appendChild( renderer.domElement );
 		
 		///////////////////////////////////////////////
 		//ミニグラフ定義
@@ -296,14 +272,12 @@ ThreeDD.main = function(){
 		}
 		
 		TWEEN.update();
-		renderer.render( scene, camera );
 		renderer2.render( scene2, camera );
 	}
 
 	function WindowResize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
 	}
 
 	function SetPosition(d, i) {
@@ -540,9 +514,6 @@ ThreeDD.main = function(){
 
 		var update = new TWEEN.Tween(this)
 			.to({}, duration * 2)
-			.onUpdate(function(){
-				renderer.render( scene, camera )
-			})
 			.onComplete(function(){
 				if(callback) callback();
 			})
@@ -941,8 +912,13 @@ ThreeDD.main = function(){
 		    type: 'GET',
 		    url: '/html/data/sample_data_graph.json',
 		    dataType: 'json',
+		    scriptCharset:'utf-8',
+		    async:false,
+		    error: function(XMLHttpRequest, textStatus, errorThrown){
+			    debugger;
+			  	console.log("aaa")  
+			  },
 		    success: function(json){
-			    	debugger;
 		        if(json.data && json.data.length > 0) {
 		        	//Loading削除
 		        	$("#divChart" + i + " .loadingGraphDiv").remove();
@@ -1331,12 +1307,9 @@ ThreeDD.main = function(){
 	        		var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 					svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
 					svg.setAttribute('class', 'loader');
-					svg.setAttribute('width', '50px');
-					svg.setAttribute('height', '50px');
-					svg.setAttribute('style', 'left:500px; opacity: .8;');
 					$("#divPanel" + i).append(svg);
 					
-					$("#divPanel" + i + " svg.loader").append(CreatePath({"class":"loader", "fill":"#FFF", "transform":"translate(25, 25)"}));
+					$("#divPanel" + i + " svg.loader").append(CreatePath({"class":"loader", "fill":colors[v.color][colors[v.color].length-1], "opacity":".5", "transform":"translate(25, 25)"}));
 					
 					var alpha = 0
 					  , pi = Math.PI
@@ -1346,10 +1319,10 @@ ThreeDD.main = function(){
 					  alpha++;
 					  alpha %= 360;
 					  var r = ( alpha * pi / 180 )
-					    , x = Math.sin( r ) * 25
-					    , y = Math.cos( r ) * - 25
+					    , x = Math.sin( r ) * 22
+					    , y = Math.cos( r ) * - 22
 					    , mid = ( alpha > 180 ) ? 1 : 0
-					    , anim = 'M 0 0 v -25 A 25 25 1 ' 
+					    , anim = 'M 0 0 v -22 A 22 22 1 ' 
 					           + mid + ' 1 ' 
 					           +  x  + ' ' 
 					           +  y  + ' z';
@@ -1385,9 +1358,8 @@ ThreeDD.main = function(){
 						});
 					}, v.realtimeSecond * 1000)
 					
+					$("#divPanel" + i).append("<div class='loader'></div>");
 					//リアルタイムアイコン作成
-					!function(){function b(b){this.converter=b.converter,this.data=b.path||b.data,this.imageData=[],this.multiplier=b.multiplier||1,this.padding=b.padding||0,this.fps=b.fps||25,this.stepsPerFrame=~~b.stepsPerFrame||1,this.trailLength=b.trailLength||1,this.pointDistance=b.pointDistance||.05,this.domClass=b.domClass||"sonic",this.backgroundColor=b.backgroundColor||"rgba(0,0,0,0)",this.fillColor=b.fillColor,this.strokeColor=b.strokeColor,this.stepMethod="string"==typeof b.step?f[b.step]:b.step||f.square,this._setup=b.setup||a,this._teardown=b.teardown||a,this._preStep=b.preStep||a,this.pixelRatio=b.pixelRatio||null,this.width=b.width,this.height=b.height,this.fullWidth=this.width+2*this.padding,this.fullHeight=this.height+2*this.padding,this.domClass=b.domClass||"sonic",this.setup()}var a=function(){},c=b.argTypes={DIM:1,DEGREE:2,RADIUS:3,OTHER:0},d=b.argSignatures={arc:[1,1,3,2,2,0],bezier:[1,1,1,1,1,1,1,1],line:[1,1,1,1]},e=b.pathMethods={bezier:function(a,b,c,d,e,f,g,h,i){a=1-a;var j=1-a,k=a*a,l=j*j,m=k*a,n=3*k*j,o=3*a*l,p=l*j;return[m*b+n*f+o*h+p*d,m*c+n*g+o*i+p*e]},arc:function(a,b,c,d,e,f){var g=(f-e)*a+e,h=[Math.cos(g)*d+b,Math.sin(g)*d+c];return h.angle=g,h.t=a,h},line:function(a,b,c,d,e){return[(d-b)*a+b,(e-c)*a+c]}},f=b.stepMethods={square:function(a,b,c,d,e){this._.fillRect(a.x-3,a.y-3,6,6)},fader:function(a,b,c,d,e){this._.beginPath(),this._last&&this._.moveTo(this._last.x,this._last.y),this._.lineTo(a.x,a.y),this._.closePath(),this._.stroke(),this._last=a}};b.prototype={calculatePixelRatio:function(){var a=window.devicePixelRatio||1,b=this._.webkitBackingStorePixelRatio||this._.mozBackingStorePixelRatio||this._.msBackingStorePixelRatio||this._.oBackingStorePixelRatio||this._.backingStorePixelRatio||1;return a/b},setup:function(){var a,b,f,g,h=this.data;this.canvas=document.createElement("canvas"),this._=this.canvas.getContext("2d"),null==this.pixelRatio&&(this.pixelRatio=this.calculatePixelRatio()),this.canvas.className=this.domClass,1!=this.pixelRatio?(this.canvas.style.height=this.fullHeight+"px",this.canvas.style.width=this.fullWidth+"px",this.fullHeight*=this.pixelRatio,this.fullWidth*=this.pixelRatio,this.canvas.height=this.fullHeight,this.canvas.width=this.fullWidth,this._.scale(this.pixelRatio,this.pixelRatio)):(this.canvas.height=this.fullHeight,this.canvas.width=this.fullWidth),this.points=[];for(var i=-1,j=h.length;++i<j;){if(a=h[i].slice(1),f=h[i][0],f in d)for(var k=-1,l=a.length;++k<l;){switch(b=d[f][k],g=a[k],b){case c.RADIUS:g*=this.multiplier;break;case c.DIM:g*=this.multiplier,g+=this.padding;break;case c.DEGREE:g*=Math.PI/180}a[k]=g}a.unshift(0);for(var m,n=this.pointDistance,o=n;o<=1;o+=n)o=Math.round(1*o/n)/(1/n),a[0]=o,m=e[f].apply(null,a),this.points.push({x:m[0],y:m[1],progress:o})}this.frame=0,this.converter&&this.converter.setup&&this.converter.setup(this)},prep:function(a){if(!(a in this.imageData)){this._.clearRect(0,0,this.fullWidth,this.fullHeight),this._.fillStyle=this.backgroundColor,this._.fillRect(0,0,this.fullWidth,this.fullHeight);var e,f,g,b=this.points,c=b.length;this.pointDistance;this._setup();for(var h=-1,i=c*this.trailLength;++h<i&&!this.stopped;)f=a+h,e=b[f]||b[f-c],e&&(this.alpha=Math.round(1e3*(h/(i-1)))/1e3,this._.globalAlpha=this.alpha,this.fillColor&&(this._.fillStyle=this.fillColor),this.strokeColor&&(this._.strokeStyle=this.strokeColor),g=a/(this.points.length-1),indexD=h/(i-1),this._preStep(e,indexD,g),this.stepMethod(e,indexD,g));return this._teardown(),this.imageData[a]=this._.getImageData(0,0,this.fullWidth,this.fullWidth),!0}},draw:function(){this.prep(this.frame)||(this._.clearRect(0,0,this.fullWidth,this.fullWidth),this._.putImageData(this.imageData[this.frame],0,0)),this.converter&&this.converter.step&&this.converter.step(this),this.iterateFrame()||this.converter&&this.converter.teardown&&(this.converter.teardown(this),this.converter=null)},iterateFrame:function(){return this.frame+=this.stepsPerFrame,!(this.frame>=this.points.length)||(this.frame=0,!1)},play:function(){this.stopped=!1;var a=this;this.timer=setInterval(function(){a.draw()},1e3/this.fps)},stop:function(){this.stopped=!0,this.timer&&clearInterval(this.timer)}},window.Sonic=b}();
-
 					var sonic = new Sonic({
 						width: 50,
 						height: 50,
@@ -1405,14 +1377,14 @@ ThreeDD.main = function(){
 								angle = Math.PI/180 * degAngle,
 								angleB = Math.PI/180 * (degAngle - 180),
 								size = i*3;
-							this._.fillStyle = '#FF7B24';
+							this._.fillStyle = colors[v.color][Math.floor(colors[v.color].length/3)];//'#FF7B24'
 							this._.fillRect(
 								Math.cos(angle) * 12.5 + (25-size/2),
 								Math.sin(angle) * 7.5 + (25-size/2),
 								size,
 								size
 							); 
-							this._.fillStyle = '#63D3FF';
+							this._.fillStyle = colors[v.color][Math.floor(colors[v.color].length/1.5)];//'#63D3FF'
 							this._.fillRect(
 								Math.cos(angleB) * 7.5 + (25-size/2),
 								Math.sin(angleB) * 12.5 + (25-size/2),
@@ -1421,7 +1393,7 @@ ThreeDD.main = function(){
 							);
 							if (point.progress == 1) {
 								this._.globalAlpha = f < .5 ? 1-f : f;
-								this._.fillStyle = '#EEE';
+								this._.fillStyle = colors[v.color][0];
 								this._.beginPath();
 								this._.arc(25, 25, 2, 0, 360, 0);
 								this._.closePath();
@@ -1432,7 +1404,7 @@ ThreeDD.main = function(){
 							['line', 0, 0, 1, 1] // stub -- not actually rendered
 						]
 					});
-					d.appendChild(sonic.canvas);
+					$("#divPanel" + i + " div.loader").append(sonic.canvas);
 					sonic.play();
 	        	}
 	        }
@@ -1565,8 +1537,8 @@ ThreeDD.main = function(){
 	var borderInside = {};
 	var theta = 0;
 	var dataCount;
-	var camera, scene, renderer;
-	var geometry, material, mesh;
+	var camera;
+	var geometry, mesh;
 	var scene2, renderer2;
 	var controls;
 	var clockAutoRotate = new THREE.Clock(false);
@@ -1644,7 +1616,7 @@ ThreeDD.main = function(){
 	$("#sb-site").attr("id", "container");
 	$("#dockbar-container").css("display", "none");
 	$("#wrapper, #add-btn-area").css("display", "none");
-	$("#container").css("background", "url('"+ MakePath("img/background.jpg") +"')");
+	$("#container").css("background", "url('"+ MakePath("image/background.jpg") +"')");
 	$("#container").css("height", "100%");
 	
 	//パネル表示スタイル変更ボタン
@@ -1657,7 +1629,7 @@ ThreeDD.main = function(){
 		
 	//ソフトバンクロゴ
 	$("body").append("<div id='sumaryDiv' style='position: absolute;bottom: 12px;right: 12px;'>");
-	var headerHtml = '<img style="height: 30px" alt="logo02" src="'+ MakePath("html/image/icon_softbank_blk.png") +'">';
+	var headerHtml = '<img style="height: 30px" alt="logo02" src="'+ MakePath("icon/icon_softbank_blk.png") +'">';
 	$("div#sumaryDiv").append(headerHtml);
 	$("div#sumaryDiv").css({"transform-origin":"right bottom 0px"});
 	
@@ -1976,7 +1948,7 @@ ThreeDD.main = function(){
 		var url = location.href.replace(/\?.*$/,"")
 		if(indexDisplayStyle == 1 || indexDisplayStyle == 2) indexDisplayStyle = 3
 		var index = ((indexDisplayStyle + 1) % Object.keys(hashDisplayStyle).length)
-		location.href = url + "?js_fast_load=0&displayStyle=" + index;
+		location.href = url + "?displayStyle=" + index;
 	}, 1200000);
 	this.finished=true;
 }
