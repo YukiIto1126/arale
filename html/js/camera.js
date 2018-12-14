6041178a7bdc43260346496fbec7ed1f9c32bca8
itoy372 <yuki.ito09@g.softbank.co.jp>
2018-12-14 16:53:27 +0900

    スタンプ送信画面実装

--- html/js/camera.js
+++ html/js/camera.js
@@ -3,19 +3,17 @@ function videoStart(){
 	const medias = {
 		audio : false,
 		video : {
-      facingMode: {
-        exact : "environment" // リアカメラにアクセス
-      }
-    }
+	      facingMode: {
+	        exact : "environment" // リアカメラにアクセス
+	      }
+	    }
 	};
 
-	debugger;
 	video  = document.getElementById("video");
 	navigator.getUserMedia(medias, successCallback, errorCallback);	
 }
 
 function successCallback(stream) {
-	debugger;
   video.srcObject = stream;
 };
 
