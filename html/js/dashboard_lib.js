1341d012bb262233a2c7939bee77227c3e66ba08
YukiIto1126 <yuki.ito1126@gmail.com>
2018-12-19 12:41:25 +0900

    文字化け対応_リスタートの文字

--- html/js/dashboard_lib.js
+++ html/js/dashboard_lib.js
@@ -903,7 +903,7 @@ function MoveCameraObject(e){
 	    //繰り返しパネル
 	    timerAutoHide = setTimeout(function(){
 	    	clockFly.stop();
-	    	$("body").append("<div id='restartStraight' style='opacity:0; border: 2px solid rgba(127, 127, 127, 0.25);position:absolute;width:575px;height:400px;background: rgba(101,101,101,0.8);color: white;text-align: center;font-size: 48px;border-radius: 8px; top:"+ (window.innerHeight-400)/2 +"px; left:"+ (window.innerWidth-575)/2 +"px;' ><span style='vertical-align: middle;line-height: 400px;'>リスタート</span></div>");
+	    	$("body").append("<div id='restartStraight' style='opacity:0; border: 2px solid rgba(127, 127, 127, 0.25);position:absolute;width:575px;height:400px;background: rgba(101,101,101,0.8);color: white;text-align: center;font-size: 48px;border-radius: 8px; top:"+ (window.innerHeight-400)/2 +"px; left:"+ (window.innerWidth-575)/2 +"px;' ><span style='vertical-align: middle;line-height: 400px;'>restart</span></div>");
 	    	d3.select("#restartStraight").transition().duration(300).style("opacity",  1).each("end", function() {
 	    		$("#restartStraight").on('mouseover', function(e){
 		        	$(this).css({
